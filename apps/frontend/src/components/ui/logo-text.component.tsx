2b48b769f042bb80dc863c9095319bc8c7a6ba43
Lidor Pahima <lidorpahima28@gmail.com>
2026-08-31 15:53:41 +0300

    Fix invisible logo text: use white-text variant on dark background

--- apps/frontend/src/components/ui/logo-text.component.tsx
+++ apps/frontend/src/components/ui/logo-text.component.tsx
@@ -3,6 +3,6 @@ import React from 'react';
 export const LogoTextComponent = () => {
   return (
     // eslint-disable-next-line @next/next/no-img-element
-    <img src="/nf-logo-full.png" alt="News Factory" height={33} className="h-[33px] w-auto" />
+    <img src="/nf-logo-full-white.png" alt="News Factory" height={33} className="h-[33px] w-auto" />
   );
 };
